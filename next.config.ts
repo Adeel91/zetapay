233e3a522af209d49ae36531355bc0b3568b0538
Muhammad Adeel <muhammad_adeel91@yahoo.com>
2026-07-03 20:52:06 +0200

    Include ZK artifacts in Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    '/api/payroll': [
+      './circuits/payroll/build/payroll_js/**',
+      './circuits/payroll/build/payroll_final.zkey',
+    ],
+    '/api/payroll/pool': [
+      './circuits/pool/build/withdraw_js/**',
+      './circuits/pool/build/deposit_js/**',
+      './circuits/pool/build/withdraw_final.zkey',
+      './circuits/pool/build/deposit_final.zkey',
+    ],
+  },
+};
 
 export default nextConfig;
